532735b9d47ee1598489bbc52f8e61f3dd349941
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2025-05-11 19:50:49 +0500

    бфс по нодам

--- run2.py
+++ run2.py
@@ -1,6 +1,5 @@
 import sys
-import collections
-import heapq
+from collections import deque
 
 keys_char = [chr(i) for i in range(ord('a'), ord('z') + 1)]
 doors_char = [k.upper() for k in keys_char]
@@ -12,62 +11,57 @@ def get_input():
     return [list(line.strip()) for line in sys.stdin]
 
 
-def get_keys_doors_robots_coord(n: int, m: int, maze: list[list[str]]) -> (list, list):
-    keys, robots = list(), set()
+def get_robots_keys_coords(n: int, m: int, maze: list[list[str]]) -> (list, list):
+    robots, keys = list(), list()
     for x in range(n):
         for y in range(m):
-            if maze[x][y] in keys_char:
-                keys.append((maze[x][y], (x, y)))
-            elif maze[x][y] == "@":
-                robots.add((x, y))
+            if maze[x][y] == '@':
+                robots.append((x, y))
+            elif maze[x][y] in keys_char:
+                keys.append((x, y))
 
-    return keys, robots
-
-
-def heuristic(x1, y1, x2, y2):
-    return max(abs(x1 - x2), abs(y1 - y2))
+    return robots, keys
 
 
-def find_best_way_len_from_a_to_b(a: (int, int), b: (int, int), n: int, m: int,
-                                  maze: list[list[str]], allowed_keys: list[str], allowed_doors: list[str]) -> int:
-    visited = set()
-    queue = []
-    heapq.heappush(queue, (0, 0, a))
-    while queue:
-        priority, length, (curr_x, curr_y) = heapq.heappop(queue)
-        if (curr_x, curr_y) in visited:
-            continue
-        visited.add((curr_x, curr_y))
-        if (curr_x, curr_y) == b:
-            return length
-        for dx, dy in directions:
-            x, y = curr_x + dx, curr_y + dy
-            if (0 <= x < n and 0 <= y < m and (x, y) not in visited and
-                    (maze[x][y] == "." or maze[x][y] in allowed_doors or maze[x][y] in allowed_keys)):
-                h = heuristic(x, y, b[0], b[1])
-                p = length + 1 + h
-                heapq.heappush(queue, (p, length + 1, (x, y)))
-    return -1
+def calculate_distances_and_needed_keys(robots: list[tuple[int, int]], keys: list[tuple[int, int]],
+                                        n: int, m: int, maze: list[list[str]]):
+    all_nodes = robots + keys
+    l = len(all_nodes)
+    node_to_id = dict(zip(all_nodes, range(l)))
+
+    distances = [[float('inf')] * l for _ in range(l)]
+    necessary_keys = [[float('inf')] * l for _ in range(l)]
+
+    for i in range(l):
+        node = all_nodes[i]
+        queue = deque([(node[0], node[1], 0, 0)])
+        visited = set()
+        while queue:
+            x, y, distance, doors = queue.popleft()
+            if (x, y) in visited:
+                continue
+            visited.add((x, y))
+            if (x, y) in all_nodes and (x, y) != node:
+                j = node_to_id[(x, y)]
+                distances[i][j] = distance
+                necessary_keys[i][j] = doors
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m and maze[nx][ny] != '#':
+                    temp = doors
+                    if maze[nx][ny] in doors_char:
+                        shift = 1 << (ord(maze[nx][ny].lower()) - ord('a'))
+                        temp |= shift
+                    queue.append((nx, ny, distance + 1, temp))
+
+    return distances, necessary_keys
 
 
 def solve(maze):
     n, m = len(maze), len(maze[0])
-    keys, robots = get_keys_doors_robots_coord(n, m, maze)
-    keys.sort()
-    allowed_keys = []
-    allowed_doors = []
-    sum = 0
-    for key, (x, y) in keys:
-        allowed_keys.append(key)
-        min_len = float('inf')
-        for robot in robots:
-            l = find_best_way_len_from_a_to_b(robot, (x, y), n, m, maze, allowed_keys, allowed_doors)
-            print(f"from {robot} to {(x, y), key} len {l}")
-            if l != -1:
-                min_len = min(min_len, l)
-        sum += min_len
-        allowed_doors.append(key.upper())
-    return sum
+    robots, keys = get_robots_keys_coords(n, m, maze)
+    distances_between_nodes, necessary_keys = calculate_distances_and_needed_keys(robots, keys, n, m, maze)
+
 
 
 def main():
